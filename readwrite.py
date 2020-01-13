788a320616012a7bc423c5c8876518d766793c72
jcharlottef <jcfaselt@gmail.com>
2020-01-13 15:45:25 -0500

    added comments to code

--- readwrite.py
+++ readwrite.py
@@ -1,12 +1,14 @@
-#reads name.txt into a variable my_name and then
-#writes a new file named hello.txt with the contents Hello, my name is <my_name>.
 
 
 
+#reads name.txt into a variable my_name
 with open('name.txt') as f:
     my_name = f.read()
 
+    
+#writes a new file named hello.txt with the contents Hello, my name is <my_name>.
+
 with open('hello.txt', 'w') as f:
     f.write('hello, my name is ' + my_name)
-    
+
 
